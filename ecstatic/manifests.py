1d98182c3f616145b661a516573dfbbe224ae3f9
Matthew Tretter <m@tthewwithanm.com>
2013-01-25 14:56:52 -0500

    Add missing import

--- ecstatic/manifests.py
+++ ecstatic/manifests.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.utils import simplejson as json
 from django.utils.functional import LazyObject
 from django.utils.importlib import import_module
