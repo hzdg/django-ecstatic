292193935c955c23a7c9d57d0bf562089e513197
Matthew Tretter <m@tthewwithanm.com>
2013-01-26 01:44:22 -0500

    Add MANIFEST_FILE to conf

--- ecstatic/conf.py
+++ ecstatic/conf.py
@@ -9,6 +9,7 @@ class EcstaticConf(AppConf):
     BUILD_INCLUDES = ['*']
     BUILD_EXCLUDES = ['CVS', '.*', '*~']
     MANIFEST = 'ecstatic.manifests.JsonManifest'
+    MANIFEST_FILE = None
     USE_MANIFEST = not settings.DEBUG
     MANIFEST_CACHE = 'ecstatic_manifest' if 'ecstatic_manifest' in settings.CACHES else 'default'
     STRICT = False
