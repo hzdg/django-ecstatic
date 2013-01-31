6efc62b40e26c6ca2e338a0a0718dbada024e905
Chris McKenzie <chrismc@hzdg.com>
2013-01-30 19:09:24 -0500

    argument to specify storage. closes #1

--- ecstatic/management/commands/createstaticmanifest.py
+++ ecstatic/management/commands/createstaticmanifest.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.files.storage import get_storage_class
 from django.core.management.base import BaseCommand
 from django.utils.datastructures import SortedDict
+from optparse import make_option
 import os
 from ...manifests import ConfiguredStaticFilesManifest
 
@@ -13,10 +15,16 @@ class Command(BaseCommand):
     by ``ECSTATIC_MANIFEST``. By default, it's a JSON file.
 
     """
+    option_list = BaseCommand.option_list + (
+        make_option('-s', '--storage', action='store',
+            dest='storage_override', type="string",
+            help='override default storage backend'),
+    )
 
     help = 'Creates a file that maps static file names to their URLs.'
 
     def handle(self, *args, **options):
+        storage_override = options.get('storage_override')
         found_files = SortedDict()
         manifest = ConfiguredStaticFilesManifest()
         manifest.clear()
@@ -34,9 +42,13 @@ class Command(BaseCommand):
                 if prefixed_path not in found_files:
                     found_files[prefixed_path] = path
 
+        if storage_override:
+            storage = get_storage_class(storage_override)
+        else:
+            storage = staticfiles_storage
         for path in found_files.values() + settings.ECSTATIC_MANIFEST_EXTRAS:
             try:
-                generate_url = staticfiles_storage.generate_url
+                generate_url = storage.generate_url
             except AttributeError:
                 raise AttributeError('%s doesn\'t define a generate_url method.'
                         ' Did you remember to extend StaticManifestMixin?')
